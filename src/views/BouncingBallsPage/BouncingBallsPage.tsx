05ffcedad0167dd40720098f94eedd9d78a4c9c3
ramiinberg <rami.inberg@gmail.com>
2022-11-12 13:09:23 +0200

    creating bounce balls components

---
+++ src/views/BouncingBallsPage/BouncingBallsPage.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+const BouncingBallsPages: FC = () => {
+  return (
+    <div className='bounce-balls'>
+      <header className='bounce-balls__header'>
+        <p>Bounce balls</p>
+      </header>
+    </div>
+  )
+}
+
+export default BouncingBallsPages
